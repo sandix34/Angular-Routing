50f98c330308a4b665b99a11a5ee562c68e9a679
sandrine <sandrinezocca@gmail.com>
2020-03-17 14:58:01 +0100

    configure the router

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomepageComponent } from './homepage/homepage.component';
+import { UserComponent } from './user/user.component'
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'home', component: HomepageComponent },
+  { path: 'users', component: UserComponent },
+  { path: '',
+    redirectTo: '/home',
+    pathMatch: 'full'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
