54dbc25dd8f5bfa9b77981f7e5f107b8332a3073
sandrine <sandrinezocca@gmail.com>
2020-03-20 14:31:13 +0100

    AcivatedRoute and Router services

--- src/app/homepage/homepage.component.ts
+++ src/app/homepage/homepage.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-homepage',
@@ -7,11 +8,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomepageComponent implements OnInit {
 
-  
-
-  constructor() { }
+  constructor(private router: Router, private activatedRoute: ActivatedRoute) { }
 
   ngOnInit(): void {
+  }
+
+  redi(){
+    // this.router.navigate(['users']);
+    this.router.navigate(['/users'], { relativeTo: this.activatedRoute });
   }
 
 }
